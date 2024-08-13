ea4ed1fa8d7b09dab0eeeb2c667ef3d4a7873289
Mohammad Abu Mualla <m2872001@gmail.com>
2024-08-13 20:55:57 +0300

    Adding the task soul

---
+++ ApplectaionDBContext/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1EF.Models;
+
+namespace Task1EF.ApplectaionDBContext
+{
+    internal class ApplicationDBContext :DbContext
+    {
+        public DbSet<Product>  Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =.;DataBase=Task1EFDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
